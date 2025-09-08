4599c1c34b6d3f318f7b76b284168ee64abc4086
machara <macharabidzia@gmail.com>
2025-09-08 17:49:39 +0400

    push

--- dist/types/components/ui/badge.d.ts
+++ dist/types/components/ui/badge.d.ts
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { type VariantProps } from "class-variance-authority";
-declare const badgeVariants: any;
+declare const badgeVariants: (props?: {
+    variant?: "default" | "destructive" | "outline" | "secondary";
+} & import("class-variance-authority/types").ClassProp) => string;
 export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> {
 }
 declare function Badge({ className, variant, ...props }: BadgeProps): import("react/jsx-runtime").JSX.Element;
