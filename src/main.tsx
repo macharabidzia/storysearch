dbb29f4f5be333b65da776a379c10fb9bb500e12
machara <macharabidzia@gmail.com>
2025-09-08 16:00:25 +0400

    new build

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { MyComponent } from 'my-package';
+import App from './App.tsx'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MyComponent />
+    <App />
   </StrictMode>,
 )
