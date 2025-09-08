375964aee3a5123634f3e81ac06ff9a7cda7d001
machara <macharabidzia@gmail.com>
2025-09-08 16:42:04 +0400

    fixed again

--- dist/App.js
+++ dist/App.js
@@ -1,4 +1,4 @@
 import { jsx as _jsx } from "react/jsx-runtime";
-export default function App() {
+export const App = () => {
     return (_jsx("div", { className: "flex items-center justify-center h-screen", children: _jsx("p", { className: "text-lg font-medium", children: "hello world" }) }));
-}
+};
